570be85ad881845aeeda9260735c82981c149059
gagan3221 <reanimatedgagan@gmail.com>
2025-07-05 18:06:52 +0530

    Apollo server added for api execution

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
-const typeDef = `#graphql
-type Query{
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Query {
     greeting: String
-}
+  }
 `;
 
-const resolver = {
+const resolvers = {
   Query: {
     greeting: () => "Hello world",
   },
 };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 9000 },
+});
+
+console.log(`🚀 Server started at: ${url}`);
